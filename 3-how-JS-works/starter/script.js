1a2524e9da144dd16ae70c5d8e40e08927a59832
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-17 14:26:48 -0300

    Hoisting, this Object, theorical content.

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,22 +1,33 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//contexto de funções é executado primeiro
+/*
+calculateAge(1989); // em execução, esta função já existe, o que torna possível o seu uso.
+
+function calculateAge(year) {
+    console.log(2019 - year);
+}
 
 
 
+//retirement(1989);//retirement is not defined
 
+//funções em variáveis, são executadas depois do contexto de funções
+var retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
 
+retirement(1989);
+*/
+//******************************************/
 
-
-
-
-
-
-
-
-
-
-
+/*
+//variables
+console.log(age); // em execução, age é undefined
+var age = 18; // age é definida
+console.log(age); // 18
+*/
 ///////////////////////////////////////
 // Lecture: Scoping
 
