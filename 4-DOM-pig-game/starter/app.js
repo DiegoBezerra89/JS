81816314c480f3cdefe53b3be983a6e68eacdf4e
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-17 17:45:36 -0300

    JS events, querySelector, document., getElementById

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,33 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//dice = Math.floor(Math.random() * 6) + 1;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-' + activePlayer).textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    //2. display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = 'block';
+});
