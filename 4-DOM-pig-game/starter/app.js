f88fb89389c10389e007a0dd5e65bbb1c5ee8cf1
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-18 17:53:14 -0300

    pig game project - Dice roll and hold.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -24,8 +24,8 @@ document.getElementById('current-1').textContent = '0';
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 
-var x = document.querySelector('#score-' + activePlayer).textContent;
-console.log(x);
+//var x = document.querySelector('#score-' + activePlayer).textContent;
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -37,4 +37,54 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDOM = document.querySelector('.dice');
     diceDOM.src = 'dice-' + dice + '.png';
     diceDOM.style.display = 'block';
+
+    //3. update the round score if the roll dice was not a 1 
+    if(dice !== 1) {
+        //add score
+        youWon();
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        //scores[activePlayer] += roundScore;
+    } else {
+        //next Player
+        nextPlayer();
+    }
 });
+
+//btn-hold
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //add to main score
+    
+    scores[activePlayer] += roundScore;
+    
+    //update the UI
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    //Check if Player won the game
+    
+    youWon();
+
+    //Next Player
+    nextPlayer();
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('#current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+function youWon() {
+    if (roundScore + scores[activePlayer] >= 30) {
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        alert('O player ' + (activePlayer + 1) + ' GANHOU!');
+    }
+}
+
