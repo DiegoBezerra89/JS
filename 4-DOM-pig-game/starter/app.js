dd20950760cc2db4d79ec2e217b23807225b30cf
unknown <Diego Bezerra>
2019-07-21 21:11:39 -0300

    Hold button, roll button, new game button DONE.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,4 +11,74 @@ GAME RULES:
 
 */
 
-document.querySelector('.dice').style.display = 'none';
+//var declarations
+var dice, scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+//reset all scores
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'none';//no dice image
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var diceDOM = document.querySelector('.dice');
+    dice = Math.floor(Math.random() * 6) + 1;
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    if(dice !== 1){
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else { 
+        //next player
+        nextPlayer();
+    }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    youWon();
+});
+
+document.querySelector('.btn-new').addEventListener('click', function(){
+    newGame();
+});
+
+
+//FUNCTIONS
+function newGame() {
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.btn-hold').style.display = 'block';
+    document.querySelector('.btn-roll').style.display = 'block'; 
+    document.getElementById('.player-name').style.color = '#555';
+    document.getElementById('name-' + activePlayer).textContent = 'Player ' + activePlayer;
+}
+
+function nextPlayer() {
+    document.getElementById('current-' + activePlayer).textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;  
+    roundScore = 0;
+}
+
+function youWon() {
+    if(scores[activePlayer] >= 30) {
+        document.getElementById('name-' + activePlayer).textContent = 'WINNER';
+        document.getElementById('name-' + activePlayer).style.color = 'red';
+        document.querySelector('.btn-hold').style.display = 'none';
+        document.querySelector('.btn-roll').style.display = 'none';       
+    } else {
+        nextPlayer();
+    }
+}
