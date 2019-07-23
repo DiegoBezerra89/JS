5db0dd7fa2fce3cf5643d7a2209da2dddaca336f
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-23 14:14:17 -0300

    All things done on dice game.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -12,11 +12,10 @@ GAME RULES:
 */
 
 //var declarations
-var dice, scores, roundScore, activePlayer;
+var dice, scores, roundScore, activePlayer, count;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+
+init();
 
 //reset all scores
 document.getElementById('score-0').textContent = '0';
@@ -33,7 +32,9 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
     if(dice !== 1){
         roundScore += dice;
+        dice === 6 ? count += 1 : '';
         document.getElementById('current-' + activePlayer).textContent = roundScore;
+        loseScore();
     } else { 
         //next player
         nextPlayer();
@@ -42,8 +43,14 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
+    noneDice();
+    //add current score to Global score
     scores[activePlayer] += roundScore;
+
+    //update the UI
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    //chack if the player won
     youWon();
 });
 
@@ -54,17 +61,22 @@ document.querySelector('.btn-new').addEventListener('click', function(){
 
 //FUNCTIONS
 function newGame() {
+    noneDice();
+    roundScore = 0;
+    scores[0,0];
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
     document.querySelector('.btn-hold').style.display = 'block';
     document.querySelector('.btn-roll').style.display = 'block'; 
-    document.getElementById('.player-name').style.color = '#555';
-    document.getElementById('name-' + activePlayer).textContent = 'Player ' + activePlayer;
+    document.querySelector('.player-name').style.color = '#555';
+    document.getElementById('name-' + activePlayer).textContent = 'Player ' + (activePlayer + 1);
 }
 
 function nextPlayer() {
+    noneDice();
+    count = 0;
     document.getElementById('current-' + activePlayer).textContent = '0';
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
@@ -74,11 +86,42 @@ function nextPlayer() {
 
 function youWon() {
     if(scores[activePlayer] >= 30) {
-        document.getElementById('name-' + activePlayer).textContent = 'WINNER';
+        document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
         document.getElementById('name-' + activePlayer).style.color = 'red';
         document.querySelector('.btn-hold').style.display = 'none';
         document.querySelector('.btn-roll').style.display = 'none';       
     } else {
+        nextPlayer();
+    }
+}
+
+function noneDice() {
+    document.querySelector('.dice').style.display = 'none';
+}
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+}
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
+
+
+//1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+
+function loseScore() {
+    if(count > 1) {
+        scores[activePlayer] = 0;
+        alert("you have got two times the number 6!");
+        document.getElementById('score-' + activePlayer).textContent = '0';
         nextPlayer();
     }
 }
