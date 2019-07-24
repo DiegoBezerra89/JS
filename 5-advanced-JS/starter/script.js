6bb6506e73b7f9d7a34b1c54d14021024986d027
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-24 16:22:48 -0300

    function as arguments

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,140 @@
+//Creating objects and cronstructors
+
+    /*
+    //literal method
+    var john = {
+        name: 'John',
+        yearOfBirth: 1990,
+        job: 'Teacher'
+    }
+
+    var Person = function(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    //method of Person
+        this.calculateAge = function() {
+        console.log(2019 - this.yearOfBirth);
+        }
+    }
+
+    //we can insert a property with the constructor prototype
+    Person.prototype.lastName = 'Smith';
+
+    var mark = new Person('Mark', 1985, 'Designer');
+
+    //we can use the prototype inheritance to insert a method to a constructor
+    Person.prototype.calculateAge = function() {
+        console.log(2019 - this.yearOfBirth);
+    }
+
+    //mark inherit the methods in the constructor prototype
+    mark.calculateAge();
+
+    console.log(mark.lastName);//Smith
+    */
+
+
+    /*********
+     * 
+     * object.create
+     * 
+     *********/
+/*
+    var personProto = {
+        calculateAge: function() {
+            console.log(2019 - this.yearOfBirth);
+        }
+    };
+
+    var john = Object.create(personProto);
+    john.name = 'John';
+    john.yearOfBirth = 1989;
+    john.job = 'Teacher';
+
+    var jane = Object.create(personProto, {
+        name: { value: 'Jane'},
+        yearOfBirth: { value: 1997 },
+        job: { value: 'Designer'}
+    });
+
+*/
+
+/*************** PRIMITIVES VS OBJECTS ***************/
+
+        /*
+        //OBJECTS
+        var obj1 = {
+            name: 'John',
+            age: 26
+        };
+
+        var obj2 = obj1;
+        obj1.age = 30;
+        console.log(obj1.age);
+        console.log(obj2.age);
+        //was created a reference to obj1 in memory, always which obj1 has change, obj2 changes too.
+
+        //FUNCTIONS
+        var age = 27;
+        var obj = {
+            name: 'Jonas',
+            city: 'Lisbon',
+        };
+
+        function change( a, b ){
+            a = 30;
+            b.city = 'San Francisco';
+        }
+
+        change(age, obj);
+
+        console.log(age);//27
+        console.log(obj.city);//san francisco
+
+        //when you declare a variable value, this turns to a primitive value, imutable, unless if you changes the value directly x = change(), it returns a result to variable, then it value changes.
+        //objects are always a pointer to an object, them could be mutated with indirect declarations
+        */
+
+
+/**************** 
+
+PASSING FUNCTIONS AS ARGUMENTS
+
+*****************/
+var years = [1990, 1987, 1964, 2007, 1999];
+
+function arrayCalc (arr, fn) {
+    var arrRes = [];
+    for(var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2019 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if(el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+
+console.log(ages);
+console.log(fullAges);
+//it's a hard concept, so, I'll need to repeat a lot of times the video
+
+
+
