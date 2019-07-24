f47d386a5971e34f7361dd1bac80ccb1b8034d81
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-24 12:11:57 -0300

    some changes.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -133,3 +133,6 @@ function inputValue() {
     var x = document.getElementById('inputValue').value;
     return x;
 }
+
+// 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+
