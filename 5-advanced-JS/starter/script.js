8fa0475b3311b37b81c15ba329b6205f3a7a4ea9
Diego Bezerra <diegobmartins21@gmail.com>
2019-07-26 17:26:07 -0300

    Quiz exercise, almost done

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -298,6 +298,7 @@ interviewJob('Designer')('Diego');
 interviewJob('nothing')('Edson');
 */
 
+/*
 var john = {
     name: 'John',
     age: 28,
@@ -331,3 +332,87 @@ johnFriendly('morning');
 var emilyFormal = john.presentation.bind(emily, 'formal');
 
 emilyFormal('night');
+*/
+
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+(function(){
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+*/
+var Question = function(ask, alternatives, correct) {
+    this.ask = ask;
+    this.alternatives = alternatives;
+    this.correct = correct;
+}
+
+
+//2. Create a couple of questions using the constructor
+var question1 = new Question('Qual é o maior time do Brasil?', ['1 - Palmeiras', '2 - Corinthians', '3 - São Paulo'], '1' );
+var question2 = new Question('Qual é a capital da Itália?',['1 - Milão', '2 - Roma', '3 - Palermo'], '2');
+var question3 = new Question('Como era conhecido o jogador de futebol, Manuel Franscisco do Santos?',['1 - Zetti', '2 - Romário', '3 - Dinei', '4 - Garrincha'], '4');
+
+
+//3. Store them all inside an array
+var questions = [question1, question2, question3];
+
+
+//4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+function randomQuestion(questionArray) {
+    var quiz = Math.floor(Math.random() * 3);
+    console.log(questionArray[quiz].ask);
+    for(var i = 0; i < questionArray[quiz].alternatives.length; i++){
+        console.log(questionArray[quiz].alternatives[i]);
+    };
+    return questionArray[quiz].correct;
+}
+
+//5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+function userAsk() {
+    return prompt('Digite o número correspondente à resposta: ');
+};
+
+//6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+function startQuiz(userAsk){
+    var correctAnswer = randomQuestion(questions);
+    var answer = userAsk();
+    answer === correctAnswer ? console.log('Certa a resposta!') : console.log('Errado!');
+    return answer;
+};
+
+//var answer = prompt(questionArray[quiz].alternatives);
+//answer === questionArray[quiz].correct ? console.log('Certa a resposta!') : console.log('Errado!');
+
+var a = '';
+
+while(a !== 'exit'){
+    a = startQuiz(userAsk);
+};
+
+//7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+
+})();
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
