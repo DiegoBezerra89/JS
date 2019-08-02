8fc9c1bca3024ca6bb3eeadada2bec93e5ba451b
Diego Bezerra <diegobmartins21@gmail.com>
2019-08-02 17:04:28 -0300

    some features on BudgetController and UIController (it's hard to understand what's happening, but let's move on!)

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -18,22 +18,24 @@ STEP 2: STRUCTURING OUR CODE WITH MODULES
     > Keep the units of code for a project both cleanly separeted and organized;
     > Encapsulate some data into privacy and expose other data publicly.
 
+    ==============================
 UI MODULE: 
     > Get input values
     > Add the new item to the UI
     > Update the UI
-
+    ==============================
 DATA MODULE:
     > Add a new item to our data structure
     > Calculate Budget
-
+    ==============================
 CONTROLLER MODULE:
-    > Add evente handler
-
-===========================================
+    > Add event handler
+    ==============================
 
 */
+//EXAMPLE TO DEMONSTRATE HOW MODULES WORKS
 
+/*
 var budgetController = (function(){
     var x = 23;
 
@@ -65,4 +67,100 @@ var controller = (function(budgetCtrl, UICtrl){
         }
     }
 })(budgetController, UIController);
+*/
+
+
+//BUDGET CONTROLLER
+var budgetController = (function(){
+    var Expense = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
+
+    var Incomes = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+})();
+
+
+//UI CONTROLLER
+var UIController = (function(){
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+    
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+            }
+        });
+    };
+
+
+    //this function is called everytime when we click on ok button, or when we press Enter
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var getInput = UICtrl.getInput();
+        console.log(getInput);
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget to the UI 
+    }
+
+    return {
+        init: function(){
+            setupEventListeners();
+        }
+    }
+  
+
+})(budgetController, UIController);
+
+controller.init();
+
 
