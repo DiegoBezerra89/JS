9a8f7903d69a7f3405abf251ab823d33a8a2bcc3
Diego Bezerra <diegobmartins21@gmail.com>
2019-08-08 17:41:44 -0300

    Some notes, nothing special

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -228,7 +228,7 @@ var UIController = (function(){
                 if(obj.percentage > 0) {
                     document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';  
                 } else {
-                    document.querySelector(DOMstrings.expensesLabel).textContent = '---';
+                    document.querySelector(DOMstrings.percentageLabel).textContent = '---';
                 }
         },
 
