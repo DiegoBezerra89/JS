a93583dad71d47d6cdea7dbc45177dd3862cb7e4
Diego Bezerra <diego.bezerra.martins@gmail.com>
2019-08-21 16:56:11 -0300

    Arrow functions ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -117,7 +117,7 @@ console.log(c); //40
 =====================
     STRINGS
 =====================
-*/
+
  //ES5
 let firstName = 'John';
 let lastName = 'Smith';
@@ -138,5 +138,98 @@ console.log(`This is ${firstName} ${lastName}, he borns in ${yearOfBirth}, and h
 const n = `${firstName} ${lastName}`;
 
 console.log(n.startsWith('J')); //it starts with 'J'? -> true
+console.log(n.endsWith('Sm')); //it ends with 'Sm'? -> false
+console.log(n.includes('oh')); //it includes ' '? -> true John Smith
+console.log(firstName.repeat(5)); //it repeats the string 5 times JohnJohnJohnJohn
+console.log(`${firstName} `.repeat(5)); //it repeats the string 5 times John John John John
+*/
 
+/*
+========================
+    ARROW FUNCTIONS
+========================
 
+const years = [1989, 1965, 1992, 1996];
+
+//ES5
+var ages5 = years.map(function(el) {
+    return 2019 - el;
+});
+console.log(ages5);
+
+//ES6
+let ages6 = years.map(el => 2019 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2019 - el}.`);
+console.log(ages6); //we can use more than one argument
+
+ages6 = years.map((el, index) => { //if we want more lines of code, we need to add the curly braces {}
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`;
+});
+console.log(ages6);
+*/
+
+/*
+========================
+    ARROW FUNCTIONS 2
+========================
+
+LEXICAL 'THIS' KEYWORD
+
+*/
+
+//ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        var self = this;//this in here, points to box5
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + self.position + ', and it is ' + self.color + '.';// if we use 'this' in here, it appoints to WINDOW object, it is a JS "Bug"
+            alert(str);
+        });
+    }
+}
+
+//box5.clickMe();
+
+//ES6
+var box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => { //agora o this aponta para o objeto acima dele
+            var str = `This is box number ${this.position}, and it is ${this.color}.`;
+            alert(str);
+        });
+    }
+}
+
+//box6.clickMe();
+
+function Person(name) {
+    this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function(friends) {
+    var arr = friends.map(function(el) {
+        return this.name + ' is friends with ' + el + '.';
+    }.bind(this));
+    console.log(arr);
+};
+
+var friends = ['Bob', 'Jane', 'Mark'];
+
+new Person('John').myFriends5(friends);
+
+//ES6
+Person.prototype.myFriends6 = function(friends) {
+    var arr = friends.map(el => `${this.name} is friends with ${el}.`);
+    console.log(arr);
+};
+
+new Person('Diego').myFriends6(friends);
